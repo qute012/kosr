bd1f416c7704a3c16e530c1cbe9e22cd18618238
qute012 <ejrwls012@gmail.com>
2021-01-11 16:52:53 +0000

    update ksponspeech.py

--- data/ksponspeech.py
+++ data/ksponspeech.py
@@ -1,16 +1,33 @@
 import json
 import yaml
 import argparse
+import os
+from tqdm import tqdm
+from features
 
-parser = argparse.ArgumentParser(description='Preparing Ksponspeech for training and evaluating')
-parser.add_argument('--path', default='/root/storage/dataset/kspon', type=str, help="dataset root directory")
-args = parser.parse_args()
+def prep_manifest(trn, root_dir):
+    with open('Ksponspeech/kspon_labels.json', 'r') as f:
+        vocab = json.load(f)
 
-root_dir = args.path
+    with open('../config/ksponspeech.yaml', 'r') as f:
+        conf = yaml.safe_load(f)
+        
+    os.makedirs('Ksponspeech/features', exist_ok=True)
+    with open(trn, 'r') as f:
+        lines = f.read().strip().split('\n')
+        
+    for line in tqdm(lines):
+        fname, script = line.split(' :: ')
+        #print(fname)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Preparing Ksponspeech for training and evaluating')
+    parser.add_argument('--path', default='/root/storage/dataset/kspon', type=str, help="dataset root directory")
+    args = parser.parse_args()
     
-with open('Ksponspeech/kspon_labels.json', 'r') as f:
-    vocab = json.load(f)
+    name = "Ksponspeech"
     
-with open('../config/ksponspeech.yaml', 'r') as f:
-    conf = yaml.load(f)
-
+    root_dir = args.path
+    for trn in ['train.trn', 'dev.trn', 'eval_clean.trn', 'eval_other.trn']:
+        prep_manifest(os.path.join(name, trn), root_dir)
