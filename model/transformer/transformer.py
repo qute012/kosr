7f618dec17bc807b85efd965978fd2fe10e909a3
qute012 <ejrwls012@gmail.com>
2021-01-12 18:39:44 +0000

    update model

--- model/transformer/transformer.py
+++ model/transformer/transformer.py
@@ -5,47 +5,15 @@ from encoder import *
 from decoder import *
 
 class Transformer(nn.Module):
-    def __init__(self, vocab_size, enc_n_layers=16, dec_n_layer=1, hidden_size=512, filter_size=2048, dropout_rate=0.1, share_target_embedding=True, has_inputs=True, src_pad_idx=None, trg_pad_idx=None):
+    def __init__(self, vocab_size, enc_n_layers=16, dec_n_layer=1, hidden_size=512, filter_size=2048, dropout_rate=0.1):
         super(Transformer, self).__init__()
-
-        self.hidden_size = hidden_size
-        self.emb_scale = hidden_size ** 0.5
-        self.has_inputs = has_inputs
-        self.src_pad_idx = src_pad_idx
-        self.trg_pad_idx = trg_pad_idx
-
-        self.t_vocab_embedding = nn.Embedding(t_vocab_size, hidden_size)
-        nn.init.normal_(self.t_vocab_embedding.weight, mean=0,
-                        std=hidden_size**-0.5)
-        self.t_emb_dropout = nn.Dropout(dropout_rate)
+        self.encoder = Encoder(hidden_size, filter_size,
+                               dropout_rate, n_layers)
+        
         self.decoder = Decoder(hidden_size, filter_size,
                                dropout_rate, n_layers)
-
-        if has_inputs:
-            if not share_target_embedding:
-                self.i_vocab_embedding = nn.Embedding(i_vocab_size,
-                                                      hidden_size)
-                nn.init.normal_(self.i_vocab_embedding.weight, mean=0,
-                                std=hidden_size**-0.5)
-            else:
-                self.i_vocab_embedding = self.t_vocab_embedding
-
-            self.i_emb_dropout = nn.Dropout(dropout_rate)
-
-            self.encoder = Encoder(hidden_size, filter_size,
-                                   dropout_rate, n_layers)
-
-        # For positional encoding
-        num_timescales = self.hidden_size // 2
-        max_timescale = 10000.0
-        min_timescale = 1.0
-        log_timescale_increment = (
-            math.log(float(max_timescale) / float(min_timescale)) /
-            max(num_timescales - 1, 1))
-        inv_timescales = min_timescale * torch.exp(
-            torch.arange(num_timescales, dtype=torch.float32) *
-            -log_timescale_increment)
-        self.register_buffer('inv_timescales', inv_timescales)
+        
+        self.initialize()
 
     def forward(self, inputs, targets):
         enc_output, i_mask = None, None
@@ -59,46 +27,26 @@ class Transformer(nn.Module):
                                                  device=targets.device)
         return self.decode(targets, enc_output, i_mask, t_self_mask, t_mask)
 
-    def encode(self, inputs, i_mask):
-        # Input embedding
-        input_embedded = self.i_vocab_embedding(inputs)
-        input_embedded.masked_fill_(i_mask.squeeze(1).unsqueeze(-1), 0)
-        input_embedded *= self.emb_scale
-        input_embedded += self.get_position_encoding(inputs)
-        input_embedded = self.i_emb_dropout(input_embedded)
+    def initialize(self):
+        # weight init
+        for p in self.parameters():
+            if p.dim() > 1:
+                if init_type == "xavier_uniform":
+                    torch.nn.init.xavier_uniform_(p.data)
+                elif init_type == "xavier_normal":
+                    torch.nn.init.xavier_normal_(p.data)
+                elif init_type == "kaiming_uniform":
+                    torch.nn.init.kaiming_uniform_(p.data, nonlinearity="relu")
+                elif init_type == "kaiming_normal":
+                    torch.nn.init.kaiming_normal_(p.data, nonlinearity="relu")
+                else:
+                    raise ValueError("Unknown initialization: " + init_type)
+        # bias init
+        for p in self.parameters():
+            if p.dim() == 1:
+                p.data.zero_()
 
-        return self.encoder(input_embedded, i_mask)
-
-    def decode(self, targets, enc_output, i_mask, t_self_mask, t_mask,
-               cache=None):
-        # target embedding
-        target_embedded = self.t_vocab_embedding(targets)
-        target_embedded.masked_fill_(t_mask.squeeze(1).unsqueeze(-1), 0)
-
-        # Shifting
-        target_embedded = target_embedded[:, :-1]
-        target_embedded = F.pad(target_embedded, (0, 0, 1, 0))
-
-        target_embedded *= self.emb_scale
-        target_embedded += self.get_position_encoding(targets)
-        target_embedded = self.t_emb_dropout(target_embedded)
-
-        # decoder
-        decoder_output = self.decoder(target_embedded, enc_output, i_mask,
-                                      t_self_mask, cache)
-        # linear
-        output = torch.matmul(decoder_output,
-                              self.t_vocab_embedding.weight.transpose(0, 1))
-
-        return output
-
-    def get_position_encoding(self, x):
-        max_length = x.size()[1]
-        position = torch.arange(max_length, dtype=torch.float32,
-                                device=x.device)
-        scaled_time = position.unsqueeze(1) * self.inv_timescales.unsqueeze(0)
-        signal = torch.cat([torch.sin(scaled_time), torch.cos(scaled_time)],
-                           dim=1)
-        signal = F.pad(signal, (0, 0, 0, self.hidden_size % 2))
-        signal = signal.view(1, max_length, self.hidden_size)
-        return signal
+        # reset some modules with default init
+        for m in self.modules():
+            if isinstance(m, (nn.Embedding, mm.LayerNorm)):
+                m.reset_parameters()
