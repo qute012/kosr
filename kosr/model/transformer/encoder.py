6eba0c369c1e8ce8abceaf482502af61427e20cb
qute012 <ejrwls012@gmail.com>
2021-01-19 07:58:03 +0000

    implement pe

--- kosr/model/transformer/encoder.py
+++ kosr/model/transformer/encoder.py
@@ -30,14 +30,16 @@ class EncoderLayer(nn.Module):
 class Encoder(nn.Module):
     def __init__(self, hidden_size, filter_size, n_head, dropout_rate, n_layers):
         super(Encoder, self).__init__()
-
+        self.first_norm = nn.LayerNorm(hidden_size, eps=1e-6)
+        self.pe = PositionalEncoding(hidden_size)
+        
         self.layers = nn.ModuleList([EncoderLayer(hidden_size, filter_size, n_head, dropout_rate)
                     for _ in range(n_layers)])
 
         self.last_norm = nn.LayerNorm(hidden_size, eps=1e-6)
 
     def forward(self, inputs, mask):
-        encoder_output = inputs
+        encoder_output = self.first_norm(inputs + self.pe(inputs.size(1)))
         for enc_layer in self.layers:
             encoder_output, mask = enc_layer(encoder_output, mask)
         return self.last_norm(encoder_output), mask
