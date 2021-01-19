33a82f3690cd2112232bde6ac05596047162d775
qute012 <ejrwls012@gmail.com>
2021-01-19 06:50:43 +0000

    update feature extractor

--- kosr/model/feature_extractor.py
+++ kosr/model/feature_extractor.py
@@ -1,6 +1,21 @@
 import torch
 import torch.nn as nn
 
+def compute_mask(mask):
+    """Create a subsampled version of x_mask.
+    Args:
+        x_mask: Input mask (B, 1, T)
+    Returns:
+        x_mask: Output mask (B, 1, sub(T))
+    """
+    t1 = mask.size(2) - (mask.size(2) % 3)
+    mask = mask[:, :, :t1][:, :, ::3]
+
+    t2 = mask.size(2) - (mask.size(2) % 2)
+    mask = mask[:, :, :t2][:, :, ::2]
+
+    return mask
+
 class VGGExtractor(nn.Module):
     def __init__(self, feature_size=128, hidden_size=512, proj_dropout_rate=0.1):
         super(VGGExtractor,self).__init__()
@@ -19,11 +34,15 @@ class VGGExtractor(nn.Module):
         self.post_extract_proj = nn.Linear(feature_size, hidden_size)
         self.dropout = nn.Dropout(p=proj_dropout_rate)
         
-    def forward(self, x):
+    def forward(self, x, mask=None):
+        x = x.unsqueeze(1)
         x = self.conv(x)
-        x = self.post_extract_proj(x)
+        B,C,T,F = x.size()
+        x = self.post_extract_proj(x.transpose(1,2).contiguous().view(B,T,C*F))
         x = self.dropout(x)
-        return x
+        if mask is None:
+            return x, mask
+        return x, compute_mask(mask)
         
 class W2VExtractor(nn.Module):
     def __init__(self, feature_size=512, hidden_size=512, conv_dropout_rate=0.0, proj_dropout_rate=0.1):
@@ -54,8 +73,12 @@ class W2VExtractor(nn.Module):
         self.post_extract_proj = nn.Linear(feature_size, hidden_size)
         self.dropout = nn.Dropout(p=proj_dropout_rate)
         
-    def forward(self, x):
+    def forward(self, x, mask):
+        x = x.unsqueeze(1)
         x = self.conv(x)
-        x = self.post_extract_proj(x)
+        B,C,T,F = x.size()
+        x = self.post_extract_proj(x.transpose(1,2).contiguous().view(B,T,C*F))
         x = self.dropout(x)
-        return x
+        if mask is None:
+            return x, mask
+        return x, compute_mask(mask)
