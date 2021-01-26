6f2cdd07db4648f249d1e0fc4783b669c3c76cf7
qute012 <ejrwls012@gmail.com>
2021-01-26 14:44:16 +0000

    update act_func

--- kosr/model/transformer/sub_layer.py
+++ kosr/model/transformer/sub_layer.py
@@ -20,7 +20,7 @@ class FeedForwardNetwork(torch.nn.Module):
     def __init__(self, hidden_size, filter_size, dropout_rate):
         super(FeedForwardNetwork, self).__init__()
         self.layer1 = nn.Linear(hidden_size, filter_size)
-        self.act = nn.SELU()
+        self.act = nn.ReLU()
         self.dropout1 = nn.Dropout(dropout_rate)
         self.layer2 = nn.Linear(filter_size, hidden_size)
         self.dropout2 = nn.Dropout(dropout_rate)
