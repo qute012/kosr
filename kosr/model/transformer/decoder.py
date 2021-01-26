4f113c510e0608444ecee4f2488f5fbb8eece690
qute012 <ejrwls012@gmail.com>
2021-01-26 14:26:17 +0000

    update decoder

--- kosr/model/transformer/decoder.py
+++ kosr/model/transformer/decoder.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 from kosr.model.attention import MultiHeadAttention, RelPositionMultiHeadAttention
-from kosr.model.transformer.sub_layer import *
+from kosr.model.transformer.sub_layer import FeedForwardNetwork
+from kosr.model.mask import target_mask
 
 class DecoderLayer(nn.Module):
     def __init__(self, hidden_dim, filter_dim, n_head, dropout_rate):
@@ -9,49 +10,43 @@ class DecoderLayer(nn.Module):
         self.att_norm = nn.LayerNorm(hidden_dim, eps=1e-6)
         self.att = MultiHeadAttention(n_head, hidden_dim, dropout_rate)
 
-        self.enc_dec_attention_norm = nn.LayerNorm(hidden_dim, eps=1e-6)
-        self.enc_dec_attention = MultiHeadAttention(n_head, hidden_dim, dropout_rate)
-        self.enc_dec_attention_dropout = nn.Dropout(dropout_rate)
+        self.memory_att_norm = nn.LayerNorm(hidden_dim, eps=1e-6)
+        self.memory_att = MultiHeadAttention(n_head, hidden_dim, dropout_rate)
 
         self.ffn_norm = nn.LayerNorm(hidden_dim, eps=1e-6)
         self.ffn = FeedForwardNetwork(hidden_dim, filter_dim, dropout_rate)
-        self.ffn_dropout = nn.Dropout(dropout_rate)
+        #self.dropout = nn.Dropout(dropout_rate)
 
-    def forward(self, x, enc_output, tgt_mask, enc_mask, cache):
-        y = self.self_attention_norm(x)
-        y = self.self_attention(y, y, y, self_mask)
-        y = self.self_attention_dropout(y)
+    def forward(self, x, x_mask, memory, memory_mask):
+        y = self.att_norm(x)
+        y = self.att(y, y, y, x_mask)
         x = x + y
 
-        if enc_output is not None:
-            y = self.enc_dec_attention_norm(x)
-            y = self.enc_dec_attention(y, enc_output, enc_output, i_mask,
-                                       cache)
-            y = self.enc_dec_attention_dropout(y)
+        if memory is not None:
+            y = self.memory_att_norm(x)
+            y = self.memory_att(y, memory, memory, memory_mask)
             x = x + y
 
         y = self.ffn_norm(x)
         y = self.ffn(y)
-        y = self.ffn_dropout(y)
         x = x + y
         return x
 
 class Decoder(nn.Module):
-    def __init__(self, hidden_dim, filter_dim, n_head, dropout_rate, n_layers):
+    def __init__(self, out_dim, hidden_dim, filter_dim, n_head, dropout_rate, n_layers):
         super(Decoder, self).__init__()
 
         self.layers = nn.ModuleList([DecoderLayer(hidden_dim, filter_dim, n_head, dropout_rate)
                     for _ in range(n_layers)])
 
         self.last_norm = nn.LayerNorm(hidden_dim, eps=1e-6)
+        self.fc = nn.Linear(hidden_dim, out_dim)
 
-    def forward(self, targets, enc_output, enc_mask, cache):
-        decoder_output = targets
+    def forward(self, tgt, memory=None, memory_mask=None):
+        tgt_mask = target_mask(tgt)
+        
+        decoder_output = tgt
         for i, dec_layer in enumerate(self.layers):
-            layer_cache = None
-            if cache is not None:
-                if i not in cache:
-                    cache[i] = {}
-                layer_cache = cache[i]
-            decoder_output = dec_layer(decoder_output, enc_output, tgt_mask, enc_mask, layer_cache)
-        return self.last_norm(decoder_output)
+            decoder_output = dec_layer(decoder_output, tgt_mask, memory, memory_mask)
+        decoder_output = self.fc(self.last_norm(decoder_output))
+        return decoder_output
