bd1690247a481d32c8c0dd0cc6875a9261c5bb83
qute012 <ejrwls012@gmail.com>
2021-01-28 15:51:39 +0000

    update checkpoint.py

---
+++ kosr/trainer/checkpoint.py
@@ -0,0 +1,9 @@
+import torch
+
+def save(name, epoch, model, optimizer, loss):
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': loss,
+    }, name)
