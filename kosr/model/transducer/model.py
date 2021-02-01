0a20041d1a2eb309623130dce07b0aead37e63fa
qute012 <ejrwls012@gmail.com>
2021-02-01 12:29:25 +0000

    update transducer

--- kosr/model/transducer/model.py
+++ kosr/model/transducer/model.py
@@ -48,27 +48,41 @@ class Transducer(nn.Module):
         self.initialize()
 
     def forward(self, inputs, input_length, tgt):
+        btz = tgt.size(0)
+        
         if self.feat_extractor == 'vgg' or self.feat_extractor == 'w2v':
             inputs,input_length = self.conv(inputs), input_length>>2
         enc_state, enc_mask = self.encoder(inputs, input_length)
-        dec_state = self.decoder(tgt)
+        dec_state = self.decoder(tgt[tgt!=self.eos_id].view(btz,-1))
         
-        pred = self.jointer(enc_state, dec_state)
-        return pred
+        preds = self.jointer(enc_state, dec_state)
+        golds = tgt[tgt!=self.sos_id].view(btz,-1)
+        
+        return preds, golds
     
     def recognize(self, inputs, input_length, tgt=None, mode='greedy'):
         if mode == 'greedy':
-            y_hats = self.greedy_search(inputs, input_length, tgt)
+            preds, golds, y_hats = self.greedy_search(inputs, input_length, tgt)
         elif mode == 'beam':
-            y_hats = self.beam_search(inputs, input_length, tgt)
-            
-        return y_hats
+            preds, golds, y_hats = self.beam_search(inputs, input_length, tgt)
+        
+        if golds is None:
+            return preds, y_hats
+        else:
+            return preds, golds, y_hats
         
     def greedy_search(self, inputs, input_length, tgt=None):
         btz = inputs.size(0)
         device = inputs.device
+        
         if tgt is None:
-            tgt = torch.zeros(btz,1, dtype=torch.long).to(device)
+            """for testing"""
+            golds = None
+            tgt = torch.zeros(btz,1, dtype=torch.long).fill_(self.sos_id).to(device)
+        else:
+            """for validation"""
+            golds = tgt[tgt!=self.sos_id].view(btz,-1)
+            tgt = tgt[:,:1]
         
         if self.feat_extractor == 'vgg' or self.feat_extractor == 'w2v':
             inputs,input_length = self.conv(inputs), input_length>>2
