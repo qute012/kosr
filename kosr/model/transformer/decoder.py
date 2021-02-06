fix layer normalization

kosr/model/transformer/decoder.py
@@ -3,19 +3,19 @@ import torch
 import torch.nn as nn
 
 from kosr.model.attention import MultiHeadAttention, RelPositionMultiHeadAttention
-from kosr.model.transformer.sub_layer import PositionalEncoding, FeedForwardNetwork
+from kosr.model.transformer.sub_layer import PositionalEncoding, FeedForwardNetwork, LayerNorm
 from kosr.model.mask import target_mask
 
 class DecoderLayer(nn.Module):
     def __init__(self, hidden_dim, filter_dim, n_head, dropout_rate):
         super(DecoderLayer, self).__init__()
-        self.att_norm = nn.LayerNorm(hidden_dim, eps=1e-6)
+        self.att_norm = LayerNorm(hidden_dim, eps=1e-6)
         self.att = MultiHeadAttention(hidden_dim, n_head, dropout_rate=0.0)
 
         self.memory_att_norm = nn.LayerNorm(hidden_dim, eps=1e-6)
         self.memory_att = MultiHeadAttention(hidden_dim, n_head, dropout_rate=0.0)
 
-        self.ffn_norm = nn.LayerNorm(hidden_dim, eps=1e-6)
+        self.ffn_norm = LayerNorm(hidden_dim, eps=1e-6)
         self.ffn = FeedForwardNetwork(hidden_dim, filter_dim, dropout_rate)
         self.dropout = nn.Dropout(dropout_rate)
 
@@ -44,7 +44,7 @@ class Decoder(nn.Module):
         self.layers = nn.ModuleList([DecoderLayer(hidden_dim, filter_dim, n_head, dropout_rate)
                     for _ in range(n_layers)])
 
-        self.last_norm = nn.LayerNorm(hidden_dim, eps=1e-6)
+        self.last_norm = LayerNorm(hidden_dim, eps=1e-6)
         self.fc = nn.Linear(hidden_dim, out_dim)
 
     def forward(self, tgt, tgt_mask=None, memory=None, memory_mask=None):

kosr/model/transformer/encoder.py
@@ -3,17 +3,17 @@ import torch
 import torch.nn as nn
 
 from kosr.model.attention import MultiHeadAttention, RelPositionMultiHeadAttention
-from kosr.model.transformer.sub_layer import PositionalEncoding, FeedForwardNetwork
+from kosr.model.transformer.sub_layer import PositionalEncoding, FeedForwardNetwork, LayerNorm
 from kosr.model.mask import make_non_pad_mask
 
 class EncoderLayer(nn.Module):
     def __init__(self, hidden_dim, filter_dim, n_head, dropout_rate):
         super(EncoderLayer, self).__init__()
-        self.att_norm = nn.LayerNorm(hidden_dim, eps=1e-6)
+        self.att_norm = LayerNorm(hidden_dim, eps=1e-6)
         #self.rel_att = RelPositionMultiHeadAttention(hidden_dim, n_head, dropout_rate)
         self.att = MultiHeadAttention(hidden_dim, n_head, dropout_rate=0.0)
 
-        self.ffn_norm = nn.LayerNorm(hidden_dim, eps=1e-6)
+        self.ffn_norm = LayerNorm(hidden_dim, eps=1e-6)
         self.ffn = FeedForwardNetwork(hidden_dim, filter_dim, dropout_rate)
         self.dropout = nn.Dropout(dropout_rate)
 
@@ -37,7 +37,7 @@ class Encoder(nn.Module):
         self.dropout = nn.Dropout(dropout_rate)
         self.layers = nn.ModuleList([EncoderLayer(hidden_dim, filter_dim, n_head, dropout_rate)
                     for _ in range(n_layers)])
-        self.norm = nn.LayerNorm(hidden_dim, eps=1e-6)
+        self.norm = LayerNorm(hidden_dim, eps=1e-6)
 
     def forward(self, inputs, mask=None):
         #mask = None

kosr/model/transformer/model.py
@@ -135,10 +135,10 @@ class Transformer(nn.Module):
     def make_in_out(self, tgt):
         btz = tgt.size(0)
         
-        #tgt_in = torch.clone(tgt)
-        #tgt_in[tgt_in==self.pad_id] = self.eos_id
-        #tgt_in = tgt_in.view(btz,-1)[:, :-1]
-        tgt_in = tgt[tgt!=self.eos_id].view(btz,-1)
+        tgt_in = torch.clone(tgt)
+        tgt_in[tgt_in==self.pad_id] = self.eos_id
+        tgt_in = tgt_in.view(btz,-1)[:, :-1]
+        #tgt_in = tgt[tgt!=self.eos_id].view(btz,-1)
         tgt_out = tgt[tgt!=self.sos_id].view(btz,-1)
         
         return tgt_in, tgt_out