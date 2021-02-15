ce4b0b2f33764c3387c9826b2d25f610d8217a19
qute012 <ejrwls012@gmail.com>
2021-02-15 14:18:38 +0000

    update params

--- kosr/trainer/checkpoint.py
+++ kosr/trainer/checkpoint.py
@@ -10,8 +10,8 @@ def save(name, epoch, model, optimizer, loss):
     }, name)
     
 def load(args, model, optimizer):
-    if args.continue_from:
-        checkpoint = torch.load(args.continue_from)
+    if args.load_model:
+        checkpoint = torch.load(args.load_model)
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         epoch = checkpoint['epoch']
